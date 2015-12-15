5e96a4361588e1e41e21c9d08be23eebb4efd32d
ialsina33 <isabellaalsina@gmail.com>
2015-12-14 23:58:14 -0500

    Final

---
+++ js/newjson.js
@@ -0,0 +1,17 @@
+(spreadsheet column names)
+stateName
+flowerName
+description
+url
+pathtoflowerphoto
+
+(spreadsheet info)
+Florida
+Orange Blossom
+Some text here
+images/flower.jpg
+
+(from LeafletIntro google doc - you have to change places)
+.bindPopup (          + '<a href="' + url + '">' + flowername + '</a>')
+
+media query - make a diff. height for smaller screen. you want map to almost fill screen. so give bigger height. no margins on the side
